a34c6cdc9ff5d35c65b18384bb73fac7685e2341
callu9 <callu_9ine@naver.com>
2024-12-16 14:06:09 +0900

    refactor: fix Pagination component to using input tag

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Container } from "../atom/Container";
 import { Icon } from "../atom/Icon";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 export interface PaginationProps {
   currentPageIndex?: number;
@@ -12,65 +13,73 @@ export default function Pagination({
   totalPageCnt = 0,
   onPageChange,
 }: PaginationProps) {
-  // const [inputValue, setValue] = useState(currentPageIndex.toString());
+  const PageInputWrapperStyle = `
+    border-radius: 8px;
+    border: 1px solid var(--gray-300, #d1d1d1);
+    :focus-within {
+      border-radius: 8px;
+      border: 1px solid var(--gray-900, #232527);
+    }
+    > input {
+      background-color: transparent;
+      text-align: center;
+      padding: 8px 10px;
+      height: 40px;
+      width: 68px;
+      outline: 0;
+    }
+  `;
   const IconWrapper = styled(Container)`
     cursor: pointer;
   `;
-  // const PageInputWrapper = styled.div`
-  //   :not(:focus-within) {
-  //     border-radius: 8px;
-  //     border: 1px solid var(--gray-300, #d1d1d1);
-  //   }
-  //   :focus-within {
-  //     border-radius: 8px;
-  //     border: 1px solid var(--gray-900, #232527);
-  //   }
-  // `;
-  // const PageInput = styled.input`
-  //   background-color: transparent;
-  //   text-align: center;
-  //   padding: 8px 10px;
-  //   height: 40px;
-  //   width: 68px;
-  //   outline: 0;
-  // `;
+  const inputRef = useRef<HTMLDivElement>(null);
+  const [inputValue, setValue] = useState(currentPageIndex + 1);
   function onChangePageIdx(newIdx: any) {
     if (isNaN(newIdx) || !onPageChange) return;
-    if (newIdx < 0) onPageChange(0);
-    else if (newIdx >= totalPageCnt) onPageChange(totalPageCnt - 1);
-    else onPageChange(newIdx);
+    let result = newIdx;
+    if (newIdx < 1) result = 1;
+    else if (newIdx > totalPageCnt) result = totalPageCnt;
+    setValue(result);
+    onPageChange(result - 1);
   }
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (!inputRef.current?.contains(e.target as HTMLElement))
+        onChangePageIdx(Number((e.target as HTMLInputElement).value));
+    };
+    window.addEventListener("mousedown", handleClick);
+    return () => window.removeEventListener("mousedown", handleClick);
+  }, [inputRef]);
   return (
-    <Container display="flex" spacing={16} align="center">
-      <IconWrapper
-        display="flex"
-        align="center"
-        onClick={() => onChangePageIdx(currentPageIndex - 1)}
-      >
-        <Icon iconNm="chevronLeft" iconColor={currentPageIndex > 0 ? "primary" : "tertiary"} />
-      </IconWrapper>
-      {/* <PageInputWrapper>
-        <PageInput
-          type="number"
-          min={1}
-          value={Number(inputValue) + 1}
-          onChange={(e: ChangeEvent) => setValue((e.target as HTMLInputElement).value)}
-          onKeyDown={(e) => e.key === "Enter" && onChangePageIdx(Number(inputValue))}
-        />
-      </PageInputWrapper> */}
-      <p>{currentPageIndex + 1}</p>
-      <p>/</p>
-      <p>{totalPageCnt}</p>
-      <IconWrapper
-        display="flex"
-        align="center"
-        onClick={() => onChangePageIdx(currentPageIndex + 1)}
-      >
-        <Icon
-          iconNm="chevronRight"
-          iconColor={totalPageCnt - 1 > currentPageIndex ? "primary" : "tertiary"}
-        />
-      </IconWrapper>
-    </Container>
+    <>
+      <style type="text/css">{`.pagination-input-wrapper {
+        ${PageInputWrapperStyle}
+      }`}</style>
+      <div className="flex-center gap-16">
+        <IconWrapper display="flex" align="center" onClick={() => onChangePageIdx(inputValue - 1)}>
+          <Icon iconNm="chevronLeft" iconColor={currentPageIndex > 0 ? "primary" : "tertiary"} />
+        </IconWrapper>
+        <div className="pagination-input-wrapper" ref={inputRef}>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e: ChangeEvent) =>
+              onChangePageIdx(
+                Number((e.target as HTMLInputElement).value.replaceAll(/[^0-9]/g, ""))
+              )
+            }
+            onKeyUp={(e) => e.key === "Enter" && onChangePageIdx(inputValue - 1)}
+          />
+        </div>
+        <p>/</p>
+        <p>{totalPageCnt}</p>
+        <IconWrapper display="flex" align="center" onClick={() => onChangePageIdx(inputValue + 1)}>
+          <Icon
+            iconNm="chevronRight"
+            iconColor={totalPageCnt - 1 > currentPageIndex ? "primary" : "tertiary"}
+          />
+        </IconWrapper>
+      </div>
+    </>
   );
 }
